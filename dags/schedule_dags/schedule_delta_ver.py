a82e527b78cd25a67f9e8f9a39da78fb5d73d16c
Fiol <fidyt.daniell@gmail.com>
2026-06-15 00:01:36 +0200

    rearrange folders for schedule-dags & add delta vere

---
+++ dags/schedule_dags/schedule_delta_ver.py
@@ -0,0 +1,32 @@
+from airflow.sdk import dag,task
+from pendulum import datetime
+from datetime import timedelta
+
+@dag(
+    dag_id = 'scheduled_dag_delta',
+    start_date = datetime(year=2026,month=6,day=14,tz='Europe/Warsaw'),
+    schedule=timedelta(days=3),
+    is_paused_upon_creation=False
+)
+def schedule_dag():
+
+    @task.python
+    def first_task():
+        print('starting scheduled dag') 
+    
+    @task.python
+    def second_task():
+        print('processing data')
+    
+    @task.python
+    def third_task():
+        print('ending scheduled dag')
+
+    
+    f1 = first_task()
+    f2=second_task()
+    f3 = third_task()
+
+    f1>>f2>>f3
+
+schedule_dag()
