3d5b236e088174d6451f17bb039b8bb5f3ba31f2
Fiol <fidyt.daniell@gmail.com>
2026-06-14 23:46:09 +0200

    add schedules w/ cron

---
+++ dags/schedule_preset_cron_ver.py
@@ -0,0 +1,31 @@
+from airflow.sdk import dag,task
+from pendulum import datetime
+
+@dag(
+    dag_id = 'scheduled_dag_cron',
+    start_date = datetime(year=2026,month=6,day=14,tz='Europe/Warsaw'),
+    schedule='0 16 * * MON-FRI',
+    is_paused_upon_creation=False
+)
+def schedule_dag():
+
+    @task.python
+    def first_task():
+        print('starting scheduled dag') 
+    
+    @task.python
+    def second_task():
+        print('processing data')
+    
+    @task.python
+    def third_task():
+        print('ending scheduled dag')
+
+    
+    f1 = first_task()
+    f2=second_task()
+    f3 = third_task()
+
+    f1>>f2>>f3
+
+schedule_dag()
