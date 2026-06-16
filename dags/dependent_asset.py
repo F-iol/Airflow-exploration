d44e354e2684313699eaec2fa2358a3b9bd2eec0
Fiol <fidyt.daniell@gmail.com>
2026-06-16 16:45:07 +0200

    add assets in dags

---
+++ dags/dependent_asset.py
@@ -0,0 +1,19 @@
+from airflow.sdk import dag,task,asset
+import pendulum
+import os
+from asset import fetch_data
+
+
+@asset(
+    schedule=fetch_data,
+    uri='/opt/airflow/logs/data/data_proccessed.txt',
+    name='process_data'
+)
+def process_data(self):
+
+    os.makedirs(os.path.dirname(self.uri),exist_ok=True)
+
+    with open(self.uri,'w') as f:
+        f.write(f'Data processed successfully at {pendulum.now('Europe/Warsaw')}')
+    
+    print(f'Data written to {self.uri}')
